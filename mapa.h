d480f3aab8b94cfe1b4f0358c70cffb253ad278a
Renan Miranda <renanmeleoterio.43@gmail.com>
2023-07-20 01:36:49 -0300

    Criando a struct mapa, e declarando a assinatura das funções

--- mapa.h
+++ mapa.h
@@ -1,8 +1,12 @@
 struct mapa
 {
-    char **matriz;
+    char ** matriz;
     int linhas;
     int colunas;
 };
 
 typedef struct mapa MAPA;
+
+void le_mapa(MAPA *m);
+void aloca_mapa(MAPA * m);
+void imprime_mapa(MAPA * m);
