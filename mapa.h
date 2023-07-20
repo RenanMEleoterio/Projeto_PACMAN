6bbac6cb9ada85ec2fac9416e6238308e53c2660
Renan Miranda <renanmeleoterio.43@gmail.com>
2023-07-20 19:33:05 -0300

    Criando struct posicao, e incluindo a assinaturas das funções libera_mapa e encontra_personagem

--- mapa.h
+++ mapa.h
@@ -1,12 +1,26 @@
+#define JOGADOR '@'
+
 struct mapa
 {
-    char ** matriz;
-    int linhas;
-    int colunas;
+    char **matriz;
+    int linha;
+    int coluna;
 };
 
 typedef struct mapa MAPA;
 
+struct posicao
+{
+    int linha;
+    int coluna;
+};
+
+typedef struct posicao POSICAO;
+
+int acabou();
 void le_mapa(MAPA *m);
-void aloca_mapa(MAPA * m);
-void imprime_mapa(MAPA * m);
+void aloca_mapa(MAPA *m);
+void libera_mapa(MAPA *m);
+void imprime_mapa(MAPA *m);
+int encontra_personagem(MAPA *m, POSICAO *posicao, char personagem);
+void anda_pelo_mapa();
