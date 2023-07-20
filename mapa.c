3d7f973e147d398867caf42dc2d568f4b4f11454
Renan Miranda <renanmeleoterio.43@gmail.com>
2023-07-20 19:31:52 -0300

    Criação das funções para limpeza de memória e de encontrar o personagem

--- mapa.c
+++ mapa.c
@@ -14,11 +14,11 @@ void le_mapa(MAPA *m){
 
     }
 
-    fscanf(f, "%d %d", &(m->linhas), &(m->colunas));
+    fscanf(f, "%d %d", &(m->linha), &(m->coluna));
 
     aloca_mapa(m);
 
-    for(int cont_linhas = 0; cont_linhas < m->linhas; cont_linhas++){
+    for(int cont_linhas = 0; cont_linhas < m->linha; cont_linhas++){
 
         fscanf(f, "%s", m->matriz [cont_linhas]);
 
@@ -28,15 +28,53 @@ void le_mapa(MAPA *m){
 }
 
 void aloca_mapa(MAPA * m){
-    m->matriz = malloc(sizeof(char*) * m->linhas);
-    for (int i = 0; i < m->linhas; i++)
+    m->matriz = malloc(sizeof(char*) * m->linha);
+    for (int i = 0; i < m->linha; i++)
     {
-        m->matriz[i]= malloc(sizeof(char) * (m->colunas + 1));
+        m->matriz[i]= malloc(sizeof(char) * (m->coluna + 1));
     }
 }
 
+void libera_mapa(MAPA *m){
+    for (int i = 0; i < m->linha; i++)
+        {
+            free(m->matriz[i]);
+        }
+        free(m->matriz);
+}
+
 void imprime_mapa(MAPA * m){
-    for(int contador_linhas = 0; contador_linhas < m->linhas; contador_linhas++){
+    for(int contador_linhas = 0; contador_linhas < m->linha; contador_linhas++){
         printf("%s\n", m->matriz[contador_linhas]);
     }
+}
+
+int acabou(){
+    return 1;
+}
+
+int encontra_personagem(MAPA *m, POSICAO *posicao, char personagem){
+
+    for(int cont_linha = 0; cont_linha < m->linha; cont_linha++){
+        for (int cont_coluna = 0; cont_coluna < m->coluna; cont_coluna++)
+        {
+            if (m->matriz[cont_linha] [cont_coluna] == personagem)
+            {
+                posicao->linha = cont_linha;
+                posicao->coluna = cont_coluna;
+
+                return 1;
+            }
+        }
+        
+    }
+
+    return 0;
+
+}
+
+void anda_pelo_mapa(MAPA *m, int linha_atual, int coluna_atual, int linha_destino, int coluna_destino){
+
+
+
 }
